af2ab297f86a535857ed1ee8bf9295917de0d2ef
agyapongeli77 <agyapongeli77@gmail.com>
2020-11-10 01:12:51 -0500

    app title added to the DOM

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+        <h1>Weather App</h1>
         <SearchBar />
       </div>
     );
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,7 +4,6 @@ import WeatherData from './WeatherData'
 import './SearchBar.css'
 import logo from './search_icon.png'
 
-let nameOfCity = "";
 class SearchBar extends Component {
     constructor(props) {
         super(props);
@@ -39,32 +38,14 @@ class SearchBar extends Component {
              //set the field to be empty after the user hits the Enter key
             this.setState({ searchItem: "" });
             console.log("Search item is...", this.state.searchItem);
-
         }
     }
-
-    // handleSubmit = async (e) => {
-
-    //     //prevent default behavior of form which refreshes 
-    //     //when button is clicked to be submitted
-    //     e.preventDefault();
-
-    //     //set the field to be empty after form has been submitted
-    //     this.setState({ searchItem: "" });
-
-    //     //call the the fetchWeatherData function 
-    //     //when the form is submitted to fetch the data
-    //     //from the API
-    //     this.fetchWeatherData();
-
-    // }
-    
 
     render() {
         return (
             <div className="SearchBar">
                 <div className="SearchBar-container">
-                    <img className="SearchBar-search_icon" src={logo}/>
+                    <img className="SearchBar-search_icon" src={logo} alt="search-icon"/>
                     <input className="SearchBar-input" type="text"
                         placeholder="Search"
                         name="searchbar"
@@ -73,7 +54,6 @@ class SearchBar extends Component {
                         onKeyUp={this.handleKeyUp}>
                     </input>
                 </div>
-
                 <WeatherData
                     itemSearched={this.state.searchItem}
                     temperature={this.state.weatherTemp}
